261b5d083d75fffbc54a731362d10974c7a7632b
Naren <narenkarthi@gmail.com>
2020-12-28 19:51:40 +0100

    after setting django app dspacing- first commit

---
+++ calc/d_spacing/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DSpacingConfig(AppConfig):
+    name = 'd_spacing'
---
+++ calc/d_spacing/views.py
@@ -0,0 +1,8 @@
+# from django.shortcuts import render
+
+from django.shortcuts import HttpResponse
+
+def index(request):
+    return HttpResponse("Hell dspacing")
+    
+# Create your views here.
