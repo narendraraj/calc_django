97025fb21e3fff2684f8e28d7536402c0f3b57cb
Naren <narenkarthi@gmail.com>
2020-12-30 01:39:34 +0100

    Model and database created

--- calc/d_spacing/admin.py
+++ calc/d_spacing/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import CrystalData
 
 # Register your models here.
+
+
+class CrystalDataAdmin(admin.ModelAdmin):
+    list_display = ('crystal_system', 'cell_lenght_a', 'cell_lenght_b',
+                    'cell_lenght_c','cell_angle_alpha', 'cell_angle_beta', 'cell_angle_gamma')
+
+
+admin.site.register(CrystalData, CrystalDataAdmin)
+
--- calc/d_spacing/models.py
+++ calc/d_spacing/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
+# # Create your models here.
 
-class Category(models.Model):
-    name = models.CharField(max_length = 128, unique = True)
-    views = models.IntegerField(default = 0)
-    likes = models.IntegerField(default = 0)
-        
-    class Meta:
-        verbose_name_plural = 'Categories'
 
+class CrystalData(models.Model):
+    crystal_system = models.CharField(max_length=25)
+    cell_lenght_a = models.DecimalField(max_digits=8, decimal_places=4, blank=True, null= True )
+    cell_lenght_b = models.DecimalField(max_digits=8, decimal_places=4,blank=True,null= True)
+    cell_lenght_c = models.DecimalField(max_digits=8, decimal_places=4, blank=True,null= True)
+    cell_angle_alpha = models.DecimalField(max_digits=8, decimal_places=4, blank=True,null= True)
+    cell_angle_beta = models.DecimalField(max_digits=8, decimal_places=4, blank=True,null= True)
+    cell_angle_gamma = models.DecimalField(max_digits=8, decimal_places=4, blank=True,null= True)
+    # space_group_name = models.CharField(null = True, max_length=25)
+    # space_group_IT_number   = models.IntegerField(null=True)
 
     def __str__(self):
-        return self.name
+        return self.crystal_system
