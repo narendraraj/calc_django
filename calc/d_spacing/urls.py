5b1e6d214974cf54ab6a3dfa88ecf6affea38989
Naren <narenkarthi@gmail.com>
2021-01-04 00:19:43 +0100

    function result_hkl_view-by_id created and working

--- calc/d_spacing/urls.py
+++ calc/d_spacing/urls.py
@@ -5,9 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('list/', views.list_view, name='home'),
+    path('list/', views.list_view, name='list'),
+    path('list/<int:crystal_id>/', views.result_hkl_view_by_id, name='list'),
     path('hkl-crystal/', views.hkl_crystal_view, name='hkl-crystal'),
-    
+
     path('home/', views.home_view, name='home'),
     path('forms/', views.crystal_data_create_view, name='submit')
 ]
--- calc/d_spacing/views.py
+++ calc/d_spacing/views.py
@@ -1,13 +1,16 @@
 # from django.shortcuts import render
 import math
-import decimal
-from django.http import HttpResponse, request
-from django.shortcuts import render
+# import decimal
+from django.http import HttpResponse, request, Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import CrystalData
 from .forms import CrystalDataForm
 
 # Create your views here.
+
+info = CrystalData.objects.get(id=10)
+
 
 def crystal_data_create_view(request):
     form = CrystalDataForm(request.POST or None)
@@ -28,7 +31,7 @@ def crystal_data_create_view(request):
 def home_view(request):
     # print(args, kwargs)
     # print(request.user)
-    info = CrystalData.objects.get(id=4)
+    # info = CrystalData.objects.get(id=11)
     context = {
 
         "object": info
@@ -47,10 +50,10 @@ def home_view(request):
 
 
 def list_view(request):
-    list_info = CrystalData.objects.all()
+    info = CrystalData.objects.all()
     context = {
 
-        "object_list": list_info
+        "object_list": info
     }
     return render(request, "crystal_list.html", context)
 
@@ -67,19 +70,22 @@ def get_d_result(crystal_structure, list_of_abc, list_of_hkl):
     if crystal_structure == 'cubic':
         result = a/(math.sqrt((h ** 2) + (k ** 2) + (l ** 2)))
     if crystal_structure == 'hexagonal':
-        result = (math.sqrt((4 / 3) * ((h ** 2) + (h * k) + (k ** 2)) / (a ** 2)) + math.sqrt((l ** 2) / (c ** 2))) ** -1
-    if crystal_structure == 'orthormobic':
-        (math.sqrt((h ** 2 / a ** 2) + (k ** 2 / b ** 2) + (l ** 2 / c ** 2))) ** -1
+        result = (math.sqrt((4 / 3) * ((h ** 2) + (h * k) + (k ** 2)
+                                       ) / (a ** 2)) + math.sqrt((l ** 2) / (c ** 2))) ** -1
+    if crystal_structure == 'orthorhombic':
+        result = (math.sqrt((h ** 2 / a ** 2) +
+                            (k ** 2 / b ** 2) + (l ** 2 / c ** 2))) ** -1
     if crystal_structure == 'tetragonal':
-        return (math.sqrt((h ** 2 + k ** 2) / a ** 2) + (l ** 2 / c ** 2)) ** -1
-    return round(result,4)
+        result = (math.sqrt((h ** 2 + k ** 2) / a ** 2) +
+                  (l ** 2 / c ** 2)) ** -1
+    return round(result, 4)
 
 
 def hkl_crystal_view(request):
-    info = CrystalData.objects.get(id=10)
+    # info = CrystalData.objects.get(id=11)
     list_of_abc = [info.cell_lenght_a, info.cell_lenght_b, info.cell_lenght_c]
-    crystal_structure= info.crystal_system
-    h_range = [1, 2, 3, 4]
+    crystal_structure = info.crystal_system
+    h_range = [1, 2, 3]
     k_range = [0, 1, 2, 3]
     l_range = [0, 1, 2, 3]
 
@@ -88,7 +94,8 @@ def hkl_crystal_view(request):
     for h in h_range:
         for k in k_range:
             for l in l_range:
-                result = get_d_result(crystal_structure, list_of_abc, [h, k, l])
+                result = get_d_result(
+                    crystal_structure, list_of_abc, [h, k, l])
                 # cubic_result = info.cell_lenght_a/decimal.Decimal((math.sqrt((h ** 2) + (k ** 2) + (l ** 2))))
                 # d_results(h,k,l)
                 # list_of_results.append([h, k, l, cubic_result])
@@ -108,6 +115,39 @@ def hkl_crystal_view(request):
     return render(request, "hkl_crystal.html", context)
 
 
+def result_hkl_view_by_id(request, crystal_id):
+    # info = CrystalData.objects.get(id=id)
+    info = get_object_or_404(CrystalData, id=crystal_id)
+    list_of_abc = [info.cell_lenght_a, info.cell_lenght_b, info.cell_lenght_c]
+    crystal_structure = info.crystal_system
+    h_range = [1, 2, 3]
+    k_range = [0, 1, 2, 3]
+    l_range = [0, 1, 2, 3]
+
+    list_of_results = []
+
+    for h in h_range:
+        for k in k_range:
+            for l in l_range:
+                result = get_d_result(
+                    crystal_structure, list_of_abc, [h, k, l])
+                # cubic_result = info.cell_lenght_a/decimal.Decimal((math.sqrt((h ** 2) + (k ** 2) + (l ** 2))))
+                # d_results(h,k,l)
+                # list_of_results.append([h, k, l, cubic_result])
+                list_of_results.append([h, k, l, result])
+
+    context = {
+        'crystal_name': info.crystal_name,
+        'crystal_formula': info.crystal_formula,
+        'crystal_system': info.crystal_system,
+        'cell_lenght_a': info.cell_lenght_a,
+        'cell_lenght_b': info.cell_lenght_b,
+        'cell_lenght_c': info.cell_lenght_c,
+        # 'cubic_result': cubic_result,
+        'list_of_results': list_of_results
+    }
+
+    return render(request, "hkl_crystal.html", context)
 
 # def cubic(request):
 
