3af2947e78106718c87fa0a2ba389f54cbd0b516
Naren <narenkarthi@gmailcom>
2023-11-12 17:17:12 +0100

    removed passwords

--- calc/calc/settings.py
+++ calc/calc/settings.py
@@ -162,5 +162,5 @@ EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'narendraraj.chandram@gmail.com'
-EMAIL_HOST_PASSWORD = 'Grapes@123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
